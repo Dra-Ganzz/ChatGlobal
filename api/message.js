ace66f3f410224ccc25e5977d1fa1d02f8000f0d
Vindra Ganzz <vindraganzz532@gmail.com>
2025-08-13 16:29:51 +0700

    Update message.js

--- api/message.js
+++ api/message.js
@@ -1,12 +1,8 @@
-let messages = global.messages || [];
-global.messages = messages;
+let messages = [];
 
 export default function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-
-  if (req.method === 'GET') {
-    return res.status(200).json(messages);
-  }
-
-  res.status(405).json({ error: 'Method not allowed' });
+  res.status(200).json(messages);
 }
+
+export { messages };
